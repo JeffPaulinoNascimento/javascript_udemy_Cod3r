2e63275121bc78483639ea9e05e01befef6528e4
Jefferson Paulino <jpnr2016@gmail.com>
2019-05-15 16:59:07 -0300

    funcões

---
+++ Função/arrowFunction.js
@@ -0,0 +1,19 @@
+let dobro = function(a){
+    return a * 2;
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+console.log(dobro(Math.PI));
+
+let ola = function(){
+    return "ola";
+}
+
+ola = () => {
+    return "ola";
+}
+
+console.log(ola());
--- Função/thisEBind3.js
+++ Função/thisEBind3.js
@@ -1,9 +1,34 @@
+/*function Pessoa(){
+    this.idade = 0;
+
+    setInterval(function(){
+        this.idade++;
+        console.log(this.idade);
+    }, 1000) // Nesse caso o this.idade não aponta para o objeto pessoa, pelo fato do this.idade++ não ser disparado pelo obejto pessoa e sim pela função
+}*/
+
+
+/*
 function Pessoa(){
     this.idade = 0;
 
     setInterval(function(){
         this.idade++;
         console.log(this.idade);
+    }.bind(this), 1000) // Nesse caso o bind(this) está apontando para o objeto pessoa
+}
+*/
+
+
+// ou assim
+
+function Pessoa(){
+    this.idade = 0;
+    const self = this;
+
+    setInterval(function(){
+        self.idade++;
+        console.log(self.idade);
     }, 1000)
 }
 
